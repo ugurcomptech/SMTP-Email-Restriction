3bfbe6677429450abfe123be5022773206afb864
Uğur <sahinugurcan1233@gmail.com>
2024-10-30 23:46:24 +0300

    Update email_monitor.sh

--- email_monitor.sh
+++ email_monitor.sh
@@ -13,7 +13,8 @@ fi
 
 # Son 1000 satırı oku ve her e-posta gönderiminde from kısmını analiz et
 tail -n 1000 "$postfix_log" | grep 'from=<.*>' | awk -F 'from=<' '{print $2}' | awk -F '>' '{print $1}' | sort | uniq -c | while read -r count email; do
-    if [[ "$count" -gt "$limit" && "$email" != "root"* ]]; then
+    # E-posta adresinin geçerli olup olmadığını kontrol et
+    if [[ -n "$email" && "$count" -gt "$limit" && "$email" != "root"* ]]; then
         # Domain kontrolü
         domain=$(echo "$email" | awk -F'@' '{print $2}')
 
@@ -28,6 +29,12 @@ tail -n 1000 "$postfix_log" | grep 'from=<.*>' | awk -F 'from=<' '{print $2}' | 
                 postmap "$header_checks_file"
                 systemctl reload postfix
                 echo "E-posta adresi başarıyla engellendi: $email" | tee -a "$log_file"
+
+                # Bildirim e-posta içeriği ve gönderimi
+                subject="E-posta Engelleme Bildirimi"
+                body="Merhaba,\n\n$email adresiniz  günlük gönderim sınırını aşması  nedeniyle engellenmiştir.\n\nSaygılarımızla."
+                echo -e "$body" | mail -s "$subject" "$email"
+                echo "Bildirim e-postası gönderildi: $email" | tee -a "$log_file"
             else
                 echo "Bu e-posta adresi zaten engellenmiş: $email" | tee -a "$log_file"
             fi
@@ -35,6 +42,6 @@ tail -n 1000 "$postfix_log" | grep 'from=<.*>' | awk -F 'from=<' '{print $2}' | 
             echo "Bu e-posta adresi engellenmedi (domain mevcut değil): $email" | tee -a "$log_file"
         fi
     else
-        echo "Root e-posta adresi engellenmedi: $email" | tee -a "$log_file"
+        echo "Root e-posta adresi engellenmedi veya geçersiz e-posta: $email" | tee -a "$log_file"
     fi
 done
