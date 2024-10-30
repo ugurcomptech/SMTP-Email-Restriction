5fb4fa3493197ae893260e7d1d8d9ac865c42fe0
Uğur <sahinugurcan1233@gmail.com>
2024-10-30 22:36:47 +0300

    Create block_mail.sh

---
+++ block_mail.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# E-posta adresini al
+read -p "Engellenecek E-posta Adresi: " email
+
+# Geçerli bir e-posta adresi kontrolü
+if [[ ! "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+    echo "Geçersiz bir e-posta adresi girdiniz."
+    exit 1
+fi
+
+# Access dosyasına ekle
+access_file="/etc/postfix/access"
+
+# Belirtilen biçimde ekleme yap
+pattern="/^From:.*$email/"
+
+if ! grep -q "$pattern" "$access_file"; then
+    echo "$pattern REJECT Custom error message: Your email is not allowed to be sent from this address." >> "$access_file"
+    # Postfix'i yeniden yükle
+    postmap "$access_file"
+    systemctl reload postfix
+    echo "E-posta adresi başarıyla engellendi: $email"
+else
+    echo "Bu e-posta adresi zaten engellenmiş."
+fi
